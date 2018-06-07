bb36129011f1f55b5365acd638710782056a0f24
Unknown <dwirandyherdinanto@gmail.com>
2018-06-08 01:36:03 +0700

    Add Repeat

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -17,53 +17,76 @@
                 <td>Prepend</td>
                 <td>{{ 'Randy' | prepend('Dwi ') }}</td>
             </tr>
+            <tr>
+                <td>Repeat</td>
+                <td>{{ 'Hello' | repeat(1) }}</td>
+            </tr>
         </table>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'FilterComponent',
-  data: function () {
-    return {
-      price: 34.23
-    }
-  },
-  filters: {
-    /**
-       * @return {string}
-       */
-    Upper (value) {
-      return value.toUpperCase()
-    },
+    export default {
+        name: 'FilterComponent',
+        data: function () {
+            return {
+                price: 34.23,
+                strHtml: '<p>Hello World</p>'
+            }
+        },
+        filters: {
+            /**
+             * @return {string}
+             */
+            Upper (value) {
+                return value.toUpperCase()
+            },
 
-    /**
-       * @return {string}
-       */
-    append (str, postifx) {
-      if (!str && str !== 0) {
-        str = ''
-      } else {
-        str = str.toString()
-      }
-      return str + postifx
-    },
-    /**
-       *
-       * @param str
-       * @param prefix
-       * @returns {string}
-       */
-    prepend (str, prefix) {
-      if (!str && str !== 0) {
-        str = ''
-      } else {
-        str = str.toString()
-      }
-      return prefix + str
+            /**
+             * @return {string}
+             */
+            append (str, postifx) {
+                if (!str && str !== 0) {
+                    str = ''
+                } else {
+                    str = str.toString()
+                }
+                return str + postifx
+            },
+            /**
+             *
+             * @param str
+             * @param prefix
+             * @returns {string}
+             */
+            prepend (str, prefix) {
+                if (!str && str !== 0) {
+                    str = ''
+                } else {
+                    str = str.toString()
+                }
+                return prefix + str
+            },
+            repeat (str, n) {
+                n = n ? Number(n) : 0
+                if (isNaN(n)) {
+                    n = 0
+                }
+
+                n = Math.floor(n)
+
+                if (n <= -1) {
+                    n = 0
+                }
+
+                var ret = ''
+                for (var i = 1; i <= n; i++) {
+                    ret += str
+                }
+                return ret
+            }
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
