dfe5c3b76f664743e6180ea2d1457abc62aff5e8
Unknown <dwirandyherdinanto@gmail.com>
2018-06-11 12:22:25 +0700

    Add String Filter
    Trim, Left Trim, Right Trim, UPPERCASE, lowercase, camelCase

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -21,11 +21,45 @@
                 <td>Repeat</td>
                 <td>{{ 'Hello' | repeat(1) }}</td>
             </tr>
+            <tr>
+                <td>Remove</td>
+                <td>{{ 'Hello World' | remove('Hello') }}</td>
+            </tr>
+            <tr>
+                <td>Split</td>
+                <td>{{ 'One-Two-Three-Four-Five' | split('-') }}</td>
+            </tr>
+            <tr>
+                <td>Trim</td>
+                <td>{{ '         Trim Left & Right     ' | trim }}</td>
+            </tr>
+            <tr>
+                <td>Trim Right</td>
+                <td>{{ '       Trim Right          ' | trimRight }}</td>
+            </tr>
+            <tr>
+                <td>Trim Left</td>
+                <td>{{ '        Trim Left        ' | trimLeft }}</td>
+            </tr>
+            <tr>
+                <td>lowercase</td>
+                <td>{{ 'LOWER CASE' | lowerCase }}</td>
+            </tr>
+            <tr>
+                <td>uppercase</td>
+                <td>{{ 'UPPER CASE' | upperCase }}</td>
+            </tr>
+            <tr>
+                <td>Camel Case</td>
+                <td>{{ 'Camel Case' | camelCase }}</td>
+            </tr>
         </table>
     </div>
 </template>
 
 <script>
+    var _ = require('underscore')
+
     export default {
         name: 'FilterComponent',
         data: function () {
@@ -38,10 +72,22 @@
             /**
              * @return {string}
              */
-            Upper (value) {
-                return value.toUpperCase()
+            upperCase (str) {
+                return str.toUpperCase()
             },
-
+            lowerCase (str) {
+                return str.toString().toLowerCase()
+            },
+            camelCase (str) {
+                return str.split(' ').map(function (word, index) {
+                    // If it is the first word make sure to lowercase all the chars.
+                    if (index === 0) {
+                        return word.toLowerCase()
+                    }
+                    // If it is not the first word only upper case the first char and lowercase the rest.
+                    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+                }).join('')
+            },
             /**
              * @return {string}
              */
@@ -67,6 +113,11 @@
                 }
                 return prefix + str
             },
+            /**
+             * @param str
+             * @param n
+             * @returns {string}
+             */
             repeat (str, n) {
                 n = n ? Number(n) : 0
                 if (isNaN(n)) {
@@ -84,6 +135,70 @@
                     ret += str
                 }
                 return ret
+            },
+            /**
+             *
+             * @param str
+             * @param substr
+             * @returns {*}
+             */
+            remove (str, substr) {
+                if (_.isString(str)) {
+                    str = str.split(substr).join('')
+                }
+                return str
+            },
+            /**
+             *
+             * @param str
+             * @param separator
+             * @returns {*}
+             */
+            split (str, separator) {
+                if (_.isString(str)) {
+                    return str.split(separator)
+                } else {
+                    return str
+                }
+            },
+            /**
+             *
+             * @param str
+             * @returns {*}
+             */
+            trim (str) {
+                var pattern = /^\\s+|\\s+$/g
+                if (_.isString(str)) {
+                    return str.replace(pattern, pattern)
+                } else {
+                    return str
+                }
+            },
+            /**
+             *
+             * @param str
+             * @returns {*}
+             */
+            trimLeft (str) {
+                var pattern = /^\\s+/
+                if (_.isString(str)) {
+                    return str.replace(pattern, pattern)
+                } else {
+                    return str
+                }
+            },
+            /**
+             *
+             * @param str
+             * @returns {*}
+             */
+            trimRight (str) {
+                var pattern = /\\s+$/
+                if (_.isString(str)) {
+                    return str.replace(pattern, pattern)
+                } else {
+                    return str
+                }
             }
         }
     }
